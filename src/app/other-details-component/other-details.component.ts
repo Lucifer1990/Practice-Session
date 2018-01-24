f878969f296d3011c556c9ec6af5c3cb310ae00b
Lucifer1990 <iamankurb@gmail.com>
2018-01-24 08:59:28 -0800

    Query params hold on child routes

--- src/app/other-details-component/other-details.component.ts
+++ src/app/other-details-component/other-details.component.ts
@@ -8,12 +8,20 @@ import { ActivatedRoute } from '@angular/router';
 
 export class ChildRouteOtherdetailsComponent implements OnInit {
     completeData: any;
+    genre: string;
+    noOfSeasons: string;
+    noOfEpisodes: string;
+    distributor: string;
 
     constructor(private _activatedRoute: ActivatedRoute) { }
 
     ngOnInit() {
         this.completeData = this._activatedRoute.parent.data.subscribe(data => {
             this.completeData = data['details'][0];
+            this.genre = this.completeData.genre;
+            this.noOfSeasons = this.completeData.no_of_seasons;
+            this.noOfEpisodes = this.completeData.no_of_episodes;
+            this.distributor = this.completeData.distributor;
         });
     }
 }
